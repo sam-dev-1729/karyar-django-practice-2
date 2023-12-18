72015c96fcef7926afb165f92dad3f6c73bd7b5c
sam-dev-1729 <sam.dev.1729@gmail.com>
2023-12-18 16:35:41 +0330

    add template

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import product_list
+from .views import product_details, product_list
 
 app_name = "shop"
 urlpatterns = [
     path("", product_list, name="list"),
+    path("<int:pk>/", product_details, name="detail"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, render
 
 from .models import Product
 
 
+@login_required
 def product_list(request):
     products = Product.objects.all()
 
     context = {"products": products}
 
-    return render(request, "product_list.html", context=context)
+    return render(request, "shop/product_list.html", context=context)
+
+
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    context = {"product": product}
+
+    return render(request, "shop/product_details.html", context=context)
